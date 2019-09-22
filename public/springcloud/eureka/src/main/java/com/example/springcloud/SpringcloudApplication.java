42ce4272541830601499501d876513283f02b653
shendejun <895017142@qq.com>
2019-09-22 09:07:52 +0800

    eureka完成

--- public/springcloud/eureka/src/main/java/com/example/springcloud/SpringcloudApplication.java
+++ public/springcloud/eureka/src/main/java/com/example/springcloud/SpringcloudApplication.java
@@ -2,10 +2,11 @@ package com.example.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@EnableEurekaServer
+@SpringBootApplication()
 public class SpringcloudApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudApplication.class, args);
     }
