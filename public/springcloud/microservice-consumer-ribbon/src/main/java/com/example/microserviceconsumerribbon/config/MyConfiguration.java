b30f207e149833c89923a8dcd4edf42f12d22fb7
shendejun <895017142@qq.com>
2019-09-22 11:11:31 +0800

    consumer-ribbon 关键

--- public/springcloud/microservice-consumer-ribbon/src/main/java/com/example/microserviceconsumerribbon/config/MyConfiguration.java
+++ public/springcloud/microservice-consumer-ribbon/src/main/java/com/example/microserviceconsumerribbon/config/MyConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class MyConfiguration {
+    
     @LoadBalanced
     @Bean
     RestTemplate loadBalanced() {
--- public/springcloud/microservice-consumer-ribbon/src/main/java/com/example/microserviceconsumerribbon/controller/HiController.java
+++ public/springcloud/microservice-consumer-ribbon/src/main/java/com/example/microserviceconsumerribbon/controller/HiController.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 @RestController
 public class HiController {
     @Autowired
