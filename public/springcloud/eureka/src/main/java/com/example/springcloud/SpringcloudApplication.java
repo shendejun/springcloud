8febf2d37b0443ff64b61e79ba606623ee6d0487
shendejun <895017142@qq.com>
2019-09-22 10:15:34 +0800

    Eureka注册中心 关键

--- public/springcloud/eureka/src/main/java/com/example/springcloud/SpringcloudApplication.java
+++ public/springcloud/eureka/src/main/java/com/example/springcloud/SpringcloudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication()
+@SpringBootApplication
 public class SpringcloudApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudApplication.class, args);
