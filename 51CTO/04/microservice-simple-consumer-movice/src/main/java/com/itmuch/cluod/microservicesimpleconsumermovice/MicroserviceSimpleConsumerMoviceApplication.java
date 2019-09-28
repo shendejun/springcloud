51083e7ca687b907ea8c5ae489f55f42cf545cde
shendejun <895017142@qq.com>
2019-09-28 22:29:44 +0800

    新建项目

---
+++ 51CTO/04/microservice-simple-consumer-movice/src/main/java/com/itmuch/cluod/microservicesimpleconsumermovice/MicroserviceSimpleConsumerMoviceApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.cluod.microservicesimpleconsumermovice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimpleConsumerMoviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSimpleConsumerMoviceApplication.class, args);
+    }
+
+}
