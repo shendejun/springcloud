705d37709ada12d92bb8293dc2c5c4afa07adec8
shendejun <895017142@qq.com>
2019-10-13 10:18:01 +0800

    和服务地址添加到配置文件中

--- 51CTO/04/microservice-simple-consumer-movice/src/main/java/com/itmuch/cluod/microservicesimpleconsumermovice/controller/MovieController.java
+++ 51CTO/04/microservice-simple-consumer-movice/src/main/java/com/itmuch/cluod/microservicesimpleconsumermovice/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.itmuch.cluod.microservicesimpleconsumermovice.controller;
 
 import com.itmuch.cluod.microservicesimpleconsumermovice.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,11 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${user.userServicePath}")
+    private String userServicePath;
+
     @GetMapping("/movie/{id}")
     public User findById(@PathVariable Long id){
-        return this.restTemplate.getForObject("http://localhost:7900/simple/"+id,User.class);
+        return this.restTemplate.getForObject(this.userServicePath+id,User.class);
     }
 }
