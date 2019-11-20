fdf166926f69c382094808eac46aa314c94bce74
shendejun <895017142@qq.com>
2019-11-21 00:15:20 +0800

    添加eureka安全验证

--- 51CTO/04-1/microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
+++ 51CTO/04-1/microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
@@ -3,6 +3,10 @@ package com.itmuch.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -11,4 +15,13 @@ public class EurekaApplication {
         SpringApplication.run(EurekaApplication.class, args);
     }
 
+    @EnableWebSecurity
+    @Configuration
+    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
+    }
 }
