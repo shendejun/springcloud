177cb31525ca17e93a760f11784034d80ef6410d
shendejun <895017142@qq.com>
2019-11-20 23:41:29 +0800

    完成课堂笔记

---
+++ 51CTO/04-1/microservice-consumer-movie/src/main/java/com/itmuch/cloud/fallback/UserClientFallback.java
@@ -0,0 +1,9 @@
+package com.itmuch.cloud.fallback;
+
+//@Component
+//public class UserClientFallback  implements UserFeignClient {
+//    @Override
+//    public String sayHi() {
+//        return "sayHi Error!!!";
+//    }
+//}
