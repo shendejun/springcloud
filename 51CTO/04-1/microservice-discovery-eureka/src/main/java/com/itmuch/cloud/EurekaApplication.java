b6be65e25756dc236586792aae7c6c522e1deb1b
shendejun <895017142@qq.com>
2019-11-21 23:56:00 +0800

    网上的教程还是解决不了请求需要输入密码验证的问题，即使输入密码，也没有请求成功

--- 51CTO/04-1/microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
+++ 51CTO/04-1/microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
@@ -1,6 +1,7 @@
 package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +14,5 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
-    }
-
-    @EnableWebSecurity
-    @Configuration
-    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-            super.configure(http);
-        }
     }
 }
