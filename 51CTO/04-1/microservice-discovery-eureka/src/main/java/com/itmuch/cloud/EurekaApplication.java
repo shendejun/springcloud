1d2fd0b2b4a877189d25094426df6186e4e8d271
shendejun <895017142@qq.com>
2019-11-21 23:59:28 +0800

    去掉安全认证

--- 51CTO/04-1/microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
+++ 51CTO/04-1/microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
@@ -1,13 +1,8 @@
 package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaServer
 @SpringBootApplication
