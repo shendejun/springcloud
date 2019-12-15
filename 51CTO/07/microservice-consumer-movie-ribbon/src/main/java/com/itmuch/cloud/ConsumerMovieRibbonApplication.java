89ba964d5d3b618777930a534a315bd741b2d91d
shendejun <895017142@qq.com>
2019-12-15 16:19:33 +0800

    同样轮询和随机的方式，这次是可以放在同一包下，但需要配置ExcludeFromComponentScan

--- 51CTO/07/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/ConsumerMovieRibbonApplication.java
+++ 51CTO/07/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/ConsumerMovieRibbonApplication.java
@@ -1,12 +1,14 @@
 package com.itmuch.cloud;
 
-import com.itmuch.config.TestConfiguration;
+import com.itmuch.cloud.config.TestConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -14,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 //这个名称一定要是尤里卡里面注册的名称
 //这个configuration不能和ConsumerMovieRibbonApplication在同一个包下
 @RibbonClient(name = "microservice-peovider-user", configuration = TestConfiguration.class)
+@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,value = ExcludeFromComponentScan.class)})
 public class ConsumerMovieRibbonApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerMovieRibbonApplication.class, args);
--- 51CTO/07/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/config/TestConfiguration.java
+++ 51CTO/07/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/config/TestConfiguration.java
@@ -1,5 +1,6 @@
-package com.itmuch.config;
+package com.itmuch.cloud.config;
 
+import com.itmuch.cloud.ExcludeFromComponentScan;
 import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
@@ -13,13 +14,14 @@ import org.springframework.context.annotation.Configuration;
  * https://cloud.spring.io/spring-cloud-static/Finchley.SR4/single/spring-cloud.html#_customizing_the_ribbon_client
  */
 @Configuration
+@ExcludeFromComponentScan
 public class TestConfiguration {
 
-    @Autowired
-    IClientConfig config;
+//    @Autowired
+//    IClientConfig config;
 
     @Bean
-    public IRule ribbonRule(IClientConfig config){
+    public IRule ribbonRule(){
         return new RandomRule();
     }
 
