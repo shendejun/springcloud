94123aba4f8931f9908f48ef90b3ecff826563f3
shendejun <895017142@qq.com>
2019-12-15 15:46:10 +0800

    使用自定义RibbonRule，实现随机轮询，TestConfiguration不能放在和Application在同一个包下

--- 51CTO/07/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/ConsumerMovieRibbonApplication.java
+++ 51CTO/07/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/ConsumerMovieRibbonApplication.java
@@ -1,14 +1,19 @@
 package com.itmuch.cloud;
 
+import com.itmuch.config.TestConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+//这个名称一定要是尤里卡里面注册的名称
+//这个configuration不能和ConsumerMovieRibbonApplication在同一个包下
+@RibbonClient(name = "microservice-peovider-user", configuration = TestConfiguration.class)
 public class ConsumerMovieRibbonApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerMovieRibbonApplication.class, args);
@@ -19,6 +24,6 @@ public class ConsumerMovieRibbonApplication {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
-    
+
 
 }
---
+++ 51CTO/07/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/config/TestConfiguration.java
@@ -0,0 +1,26 @@
+package com.itmuch.config;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * 参考地址：
+ * https://cloud.spring.io/spring-cloud-static/Finchley.SR4/single/spring-cloud.html#_customizing_the_ribbon_client
+ */
+@Configuration
+public class TestConfiguration {
+
+    @Autowired
+    IClientConfig config;
+
+    @Bean
+    public IRule ribbonRule(IClientConfig config){
+        return new RandomRule();
+    }
+
+}
